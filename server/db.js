4411725bec8eeccab8e07f1d9b3301b24aa1e857
Lauren Hart <laurenannehart@gmail.com>
2018-09-25 08:07:50 +1200

    added getBets in db.js and in routes/bets.js

--- server/db.js
+++ server/db.js
@@ -1,8 +1,14 @@
+const config = require('../knexfile').development
+const connection = require('knex')(config)
+
 module.exports = {
-  getFruits
+  getBets
 }
 
-function getFruits () {
-  const fruits = ['banana', 'apple', 'feijoa']
-  return Promise.resolve(fruits)
+// The server has asked the database to return
+// the selected columns from the bets table
+function getBets (testDb) {
+  const db = testDb || connection
+  return db('bets')
+    .select('id', 'person', 'couple', 'sport', 'bet', 'amount_bet as amountBet', 'amount_won as amountWon', 'percentage')
 }
---
+++ server/routes/bets.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const db = require('../db')
+
+const router = express.Router()
+
+router.use(express.json())
+
+// API client has requested getBets from server
+// we are requesting a response from the db now
+// we want the response to be a json object
+router.get('/', (req, res) => {
+  db.getBets()
+    .then(bets => {
+      res.json({bets})
+    })
+})
+
+module.exports = router
