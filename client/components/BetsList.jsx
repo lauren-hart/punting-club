aea4525da8e72c6f5f4bd628bca561bfef388ff5
Lauren Hart <laurenannehart@gmail.com>
2018-10-06 14:20:00 +1300

    changed BetsList component into a class component

--- client/components/BetsList.jsx
+++ client/components/BetsList.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 
-const BetsList = (props) => {
-  const amountBet = Number(props.list.amountBet).toFixed(2)
-  const amountWon = isNaN(props.list.amountWon)
-    ? Number(0).toFixed(2)
-    : Number(props.list.amountWon).toFixed(2)
-  const percentage = isNaN(((amountWon / amountBet) * 100))
-    ? Number(0) : ((amountWon / amountBet) * 100).toFixed(0)
+class BetsList extends React.Component {
+  constructor (props) {
+    super(props)
+  }
 
-  return (
-    <tr>
-      <td>{props.list.person}</td>
-      <td>{props.list.bet}</td>
-      <td>${amountBet}</td>
-      <td>${amountWon}</td>
-      <td style={props.percentColour(percentage)}>{percentage}%</td>
-      <td>{props.list.date}</td>
-      <td><button>x</button></td>
-      <td><button>Edit</button></td>
-    </tr>
-  )
+  render () {
+    const amountBet = Number(this.props.list.amountBet).toFixed(2)
+    const amountWon = isNaN(this.props.list.amountWon)
+      ? Number(0).toFixed(2)
+      : Number(this.props.list.amountWon).toFixed(2)
+    const percentage = isNaN(((amountWon / amountBet) * 100))
+      ? Number(0) : ((amountWon / amountBet) * 100).toFixed(0)
+    return (
+      <tr>
+        <td>{this.props.list.person}</td>
+        <td>{this.props.list.bet}</td>
+        <td>${amountBet}</td>
+        <td>${amountWon}</td>
+        <td style={this.props.percentColour(percentage)}>{percentage}%</td>
+        <td>{this.props.list.date}</td>
+        {/* <td><button onClick={this.betprops.handleDelete}>x</button></td> */}
+        <td><button>Delete</button></td>
+      </tr>
+    )
+  }
 }
 
 export default BetsList
