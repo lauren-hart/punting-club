ccae2022f53d5b07607927206d8354c244b72d07
Lauren Hart <laurenannehart@gmail.com>
2018-10-08 14:24:04 +1300

    removed edit bet for now, will add it back in later

--- client/components/BetsList.jsx
+++ client/components/BetsList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import {deleteBet, editBet} from '../actions/index'
+import {deleteBet} from '../actions/index'
 import {connect} from 'react-redux'
-// import Editbet from './Editbet'
 // import {Redirect} from 'react-router-dom'
 
 class BetsList extends React.Component {
@@ -11,18 +10,10 @@ class BetsList extends React.Component {
       editStatus: false
     }
     this.handleDelete = this.handleDelete.bind(this)
-    this.handleEdit = this.handleEdit.bind(this)
   }
 
   handleDelete (e) {
     this.props.dispatch(deleteBet(e.target.value))
-  }
-
-  handleEdit (e) {
-    this.setState({
-      editStatus: !this.state.edit
-    })
-    this.props.dispatch(editBet(e.target.value, this.state))
   }
 
   render () {
@@ -34,33 +25,18 @@ class BetsList extends React.Component {
       ? Number(0) : ((amountWon / amountBet) * 100).toFixed(0)
     return (
       <tbody>
-        {this.state.editStatus
-          ? <tr>
-            <td><input value={this.state.couple} placeholder={this.props.list.couple}></input></td>
-            <td><input value={this.state.person} placeholder={this.props.list.person}></input></td>
-            <td><input value={this.state.bet} placeholder={this.props.list.bet}></input></td>
-            <td><input value={this.state.amountBet} placeholder={amountBet}></input></td>
-            <td><input value={this.state.amountWon} placeholder={amountWon}></input></td>
-            <td></td>
-            <td><input value={this.state.date} placeholder={this.props.list.date}></input></td>
-            <td></td>
-            <td><button>Finish</button></td>
-
-          </tr>
-          : <tr>
-            <td>{this.props.list.couple}</td>
-            <td>{this.props.list.person}</td>
-            <td>{this.props.list.bet}</td>
-            <td>${amountBet}</td>
-            <td>${amountWon}</td>
-            <td style={this.props.percentColour(percentage)}>{percentage}%</td>
-            <td>{this.props.list.date}</td>
-            <td><button value={this.props.list.id} onClick={this.handleDelete}>x</button></td>
-            <td><button value={this.props.list.id} onClick={this.handleEdit}>Edit</button></td>
-          </tr>
-        }
+        <tr>
+          <td>{this.props.list.couple}</td>
+          <td>{this.props.list.person}</td>
+          <td>{this.props.list.bet}</td>
+          <td>${amountBet}</td>
+          <td>${amountWon}</td>
+          <td style={this.props.percentColour(percentage)}>{percentage}%</td>
+          <td>{this.props.list.date}</td>
+          <td><button value={this.props.list.id} onClick={this.handleDelete}>x</button></td>
+          <td><button value={this.props.list.id} onClick={this.handleEdit}>Edit</button></td>
+        </tr>
       </tbody>
-
     )
   }
 }
