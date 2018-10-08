e71016f3df93e43d6ad357365529dc2cf3b5224e
Lauren Hart <laurenannehart@gmail.com>
2018-10-09 12:37:44 +1300

    added Members component to Router and Route on App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,8 +4,8 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 import Home from './Home'
 import Bets from './Bets'
 import Addbet from './AddBet'
-// import Members from './Members'
-// import Leaderboard from './Leaderboard'
+import Members from './Members'
+import Leaderboard from './Leaderboard'
 // import EditBet from './EditBet'
 
 export default class App extends React.Component {
@@ -17,9 +17,9 @@ export default class App extends React.Component {
             <Route exact path='/' component={Home}/>
             <Route exact path='/bets' component={Bets} />
             <Route path='/addbet' component={Addbet}/>
-            {/* <Route path='/members' component={Members}/>
-          <Route exact path='/' component={Leaderboard}/>
-          <Route exact path='/editbet' component={EditBet}/> */}
+            <Route path='/members' component={Members}/>
+            <Route exact path='/' component={Leaderboard}/>
+            {/* <Route exact path='/editbet' component={EditBet}/> */}
           </div>
         </Router>
       </div>
