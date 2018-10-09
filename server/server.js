27f2a5df149a044d9740f49cf9ce271f9dec1b4e
Lauren Hart <laurenannehart@gmail.com>
2018-10-09 14:00:51 +1300

    updated server.js members routes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const path = require('path')
 const express = require('express')
 
 const bets = require('./routes/bets')
+const members = require('./routes/members')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/bets', bets)
+server.use('/api/v1/members', members)
 
 module.exports = server
