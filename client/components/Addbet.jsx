52dadde7554621adf7427aab985ce0a45e31297d
Lauren Hart <laurenannehart@gmail.com>
2018-10-14 20:51:15 +1300

    addbet is now working

--- client/components/Addbet.jsx
+++ client/components/Addbet.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {addBet} from '../actions/bets'
+import {Link} from 'react-router-dom'
 
 export class Addbet extends React.Component {
   constructor (props) {
@@ -12,6 +13,7 @@ export class Addbet extends React.Component {
       bet: '',
       amountBet: '',
       amountWon: '',
+      date: '',
       percentage: '',
       submitted: false
     }
@@ -39,22 +41,31 @@ export class Addbet extends React.Component {
 
   render () {
     return (
-      <table className="addbet">
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td><input placeholder='couple'></input></td>
-            <td><input placeholder='person'></input></td>
-            <td><input placeholder='bet'></input></td>
-            <td><input placeholder='amount bet'></input></td>
-            <td><input placeholder='amount won'></input></td>
-            <td></td>
-            <td><input placeholder='date'></input></td>
-            <td></td>
-            <td><button onClick={this.handleClick}>Add</button></td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <div className="bg-grass"></div>
+        <h1 className="header">Bets</h1>
+        <div className="nav">
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/bets"><button>Bets</button></Link>
+          <Link to="/members"><button>Members</button></Link>
+        </div>
+        <table className="addbet">
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td><input onChange={this.handleChange} name="couple" value={this.state.couple} placeholder='couple'></input></td>
+              <td><input onChange={this.handleChange} name="person" value={this.state.person} placeholder='person'></input></td>
+              <td><input onChange={this.handleChange} name="bet" value={this.state.bet} placeholder='bet'></input></td>
+              <td><input onChange={this.handleChange} name="amountBet" value={this.state.amountBet} placeholder='amount bet'></input></td>
+              <td><input onChange={this.handleChange} name="amountWon" value={this.state.amountWon} placeholder='amount won'></input></td>
+              <td></td>
+              <td><input onChange={this.handleChange} name="date" value={this.state.date} placeholder='date'></input></td>
+              <td></td>
+              <td><Link to="/bets"><button onClick={this.handleClick}>Add</button></Link></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
