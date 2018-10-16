ab76f39cdfe8c0f9ba628365fdaad96251e2e128
Lauren Hart <laurenannehart@gmail.com>
2018-10-17 12:13:52 +1300

    import users and server.use users.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const bets = require('./routes/bets')
 const members = require('./routes/members')
+const users = require('./routes/users')
 
 const server = express()
 
@@ -10,5 +11,6 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/bets', bets)
 server.use('/api/v1/members', members)
+server.use('/api/v1/users', users)
 
 module.exports = server
