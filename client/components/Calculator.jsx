8859b6e25f876e86542beacbf4604e08d4253fcc
Lauren Hart <laurenannehart@gmail.com>
2018-10-21 11:42:38 +1300

    created empty calculator component

--- client/components/Calculator.jsx
+++ client/components/Calculator.jsx
@@ -1,1 +1,25 @@
-q
+import React from 'react'
+import {connect} from 'react-redux'
+
+class Calculator extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    rawBets: state.bets.rawBets
+  }
+}
+
+export default connect(mapStateToProps)(Calculator)
