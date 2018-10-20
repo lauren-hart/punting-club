0bd05ccde2bf256faf7cc9b30cc0b8d3f7e309bc
Lauren Hart <laurenannehart@gmail.com>
2018-10-21 11:59:01 +1300

    added calculator to a class component

--- client/components/Calculator.jsx
+++ client/components/Calculator.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const Calculator = () => (
-  <div>
-    <div className="bg-grass"></div>
-    <h1>Hi</h1>
-  </div>
-)
+class Calculator extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <div>
+        <div className="bg-grass"></div>
+
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
 
 export default Calculator
