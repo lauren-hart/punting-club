66405f2c4e26e3acf42b14c5452699159c9c1aca
Lauren Hart <laurenannehart@gmail.com>
2018-10-25 09:53:03 +1300

    converted Balance.jsx into a class component

--- client/components/Balance.jsx
+++ client/components/Balance.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import {connect} from 'react-redux'
+// import {accounting} from 'accounting'
 
-const Balance = () => (
-  <div className="container-fluid">
-    <h4 className="balance">Balance $</h4>
-  </div>
-)
+class Balance extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
 
-export default Balance
+  render () {
+    return (
+      <div>
+        <h4 className="balance">Balance $</h4>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    rawBets: state.bets.rawBets
+  }
+}
+
+export default connect(mapStateToProps)(Balance)
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -33117,25 +33117,61 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactRedux = __webpack_require__(4);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var Balance = function Balance() {
-  return _react2.default.createElement(
-    "div",
-    { className: "container-fluid" },
-    _react2.default.createElement(
-      "h4",
-      { className: "balance" },
-      "Balance $"
-    )
-  );
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// import {accounting} from 'accounting'
+
+var Balance = function (_React$Component) {
+  _inherits(Balance, _React$Component);
+
+  function Balance(props) {
+    _classCallCheck(this, Balance);
+
+    var _this = _possibleConstructorReturn(this, (Balance.__proto__ || Object.getPrototypeOf(Balance)).call(this, props));
+
+    _this.state = {};
+    return _this;
+  }
+
+  _createClass(Balance, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'h4',
+          { className: 'balance' },
+          'Balance $'
+        )
+      );
+    }
+  }]);
+
+  return Balance;
+}(_react2.default.Component);
+
+var mapStateToProps = function mapStateToProps(state) {
+  return {
+    rawBets: state.bets.rawBets
+  };
 };
 
-exports.default = Balance;
+exports.default = (0, _reactRedux.connect)(mapStateToProps)(Balance);
 
 /***/ })
 /******/ ]);
